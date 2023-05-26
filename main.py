ee28109d50550c3c197364cb22691095e2d03e7c
Kfir Yehezkel <kfir.ye@gmail.com>
2023-05-27 02:52:01 +0300

    added latency to iot hub report. updated images of current telemetries.

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def send_telemetry(con_string, telemetry):
     msg.content_type = "application/json"
     msg.custom_properties["conn_down_speed"] = telemetry.download_speed
     msg.custom_properties["conn_up_speed"] = telemetry.upload_speed
+    msg.custom_properties["ping_latency"] = telemetry.ping_latency
     device_client.send_message(msg)
     device_client.shutdown()
 
 class Telemetry:
     def __init__(self):
         pass
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -26,15 +26,18 @@ if __name__ == '__main__':
     con_string = dic['connection_string']
     result = subprocess.run(['speedtest', '--format', 'json'], capture_output=True)
 
-    line = result.stdout.decode()
+    line = result.stdout.decode().strip()
 
     with open(dic['logfile'], "a") as file_object:
         file_object.write(line)
+        file_object.write('\n')
 
     output = json.loads(line)
     telemetry = Telemetry()
     telemetry.download_speed = int(output['download']['bandwidth']) / (1024 * 1024) * 8
     telemetry.upload_speed = int(output['upload']['bandwidth']) / (1024 * 1024) * 8
+    telemetry.ping_latency = float(output['ping']['latency'])
     send_telemetry(con_string, telemetry)
     print(f"Download speed: {telemetry.download_speed} Mbps")
     print(f"Upload speed: {telemetry.upload_speed} Mbps")
+    print(f"Ping Latency: {telemetry.ping_latency} mSec")
